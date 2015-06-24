febeef6c491b8121ae099d5c4ace75aacfa37cd5
Xuebin Dong <xudong@redhat.com>
2015-06-24 11:10:51 +0800

    Append tool-btn animation

--- inside-dom-vs-outside-dom/js/index.js
+++ inside-dom-vs-outside-dom/js/index.js
@@ -11,13 +11,13 @@
     // switch viewport
     $('.feature').on('click', '.btn-toggle', function() {
       var $this  = $(this);
-      if ($this.is('.fa-square')) {
-        $this.removeClass('fa-square').addClass('fa-th-large');
+      if ($this.is('.fa-square-o')) {
+        $this.removeClass('fa-square-o').addClass('fa-th-large');
         $this.closest('.feature-wrapper').removeClass('col-md-6 ').addClass('col-md-12')
           .siblings().removeClass('show').addClass('hidden');
         $this.closest('.feature-pk').siblings().removeClass('show').addClass('hidden');
       } else {
-        $this.removeClass('fa-th-large').addClass('fa-square');
+        $this.removeClass('fa-th-large').addClass('fa-square-o');
         $this.closest('.feature-wrapper').removeClass('col-md-12 ').addClass('col-md-6')
           .siblings().removeClass('hidden').addClass('show');
         $this.closest('.feature-pk').siblings().removeClass('hidden').addClass('show');
