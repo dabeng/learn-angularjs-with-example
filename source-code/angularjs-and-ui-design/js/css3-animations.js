1f71999d2e5f918becdf83425257cd76e558afaa
dabeng <zhexiany@gmail.com>
2015-07-31 14:43:26 +0800

    Append the example of controller communication

--- source-code/angularjs-and-ui-design/js/css3-animations.js
+++ source-code/angularjs-and-ui-design/js/css3-animations.js
@@ -2,8 +2,7 @@ app.controller('CssAnimationCtrl', function($scope, $element) {
     // customize the style of UI elements on toolbar
     $scope.toggleTips = function($event) {
       $scope.$emit('openTips', $event);
-    }
-
+    };
 
     $element.find('code').each(function(i, block) {
       hljs.highlightBlock(block);
--- source-code/core-stuff-and-cool-stuff/controller-scope/controller-communication.js
+++ source-code/core-stuff-and-cool-stuff/controller-scope/controller-communication.js
@@ -3,10 +3,72 @@ app.controller('ControllerCommunicationCtrl', function($scope, $element) {
   $scope.showCode = false;
   $scope.toggleCodeTip = 'source code';
 
+  $scope.toggleTips = function($event) {
+    $scope.$emit('openTips', $event);
+  };
+
   $scope.toggleCode = function() {
+    if (!$element.find('.hljs').length) {
+      angular.forEach($element.find('code'), function(block, index) {
+        hljs.highlightBlock(block);
+      });
+    }
     this.showCode = !this.showCode;
     this.toggleCodeTip = this.showCode ? 'example' : 'source code';
   };
 
+});
 
+app.controller('SelfCtrl', function($scope) {
+  $scope.makeInvitation = function () {
+    $scope.$broadcast('to-child', 'Child');
+    $scope.$emit('to-parent', 'Parent');
+    $scope.$emit('to-brother', 'Brother');
+  };
+});
+
+app.controller('ParentCtrl', function($scope) {
+  $scope.myResponse = 'Parent Node';
+
+  $scope.$on('to-parent', function(event,data) {
+    $scope.myResponse = data + ': anytime is OK!';
+  });
+
+  $scope.$on('to-brother', function(event,data) {
+    $scope.$broadcast('to-another-child', data);
+  });
+
+  $scope.reset = function() {
+    $scope.myResponse = 'Parent Node';
+    $scope.$broadcast('reset');
+  };
+
+  // the following code snippets alse work, but it's redundant
+  // $scope.$on('reset', function(event,data) {
+  //   $scope.myResponse = 'Parent Node';
+  // });
+});
+
+app.controller('ChildCtrl', function($scope){
+  $scope.myResponse = 'Child Node';
+
+  $scope.$on('to-child', function(event,data) {
+    $scope.myResponse = data + ': anytime is OK!';
+  });
+
+  $scope.$on('reset', function(event,data) {
+    $scope.myResponse = 'Child Node';
+  });
+});
+
+app.controller('BrotherCtrl', function($scope){  
+	$scope.myResponse = 'Brother Node';
+
+  $scope.$on('to-another-child', function(event,data) {  
+    $scope.myResponse = data + ': anytime is OK!';
+  });
+
+  $scope.$on('reset', function(event,data) {
+    $scope.myResponse = 'Brother Node';
+  });
 });
