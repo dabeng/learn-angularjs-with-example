3ebb702795261c0d977d0b1e400d3339c47d02c2
dabeng <zhexiany@gmail.com>
2015-08-19 18:03:25 +0800

    accomplish custom directive example

--- core-stuff-and-cool-stuff/directive/tag-pool.directive.js
+++ core-stuff-and-cool-stuff/directive/tag-pool.directive.js
@@ -3,28 +3,59 @@
 angular.module('myApp').directive('tagPool', function() {
   return {
     restrict: 'E',
+    replace: true,
     scope: {
       datasource: '='
     },
     templateUrl: 'tag-pool.html',
-    link: function(scope, iElement, iAttrs) {
-      iElement.on('click', '.tp-tag', function() {
-        var clickedTag = angular.element(this);
-        if (clickedTag.is('.focus')) {
-          clickedTag.removeClass('focus');
+    controller: function($scope, $element, $attrs) {
+      $scope.selectTag = function($event) {
+        var clickedTag = angular.element($event.target);
+        if (clickedTag.is('.selected')) {
+          clickedTag.removeClass('selected');
         } else {
-          clickedTag.addClass('focus');
+          clickedTag.addClass('selected');
         }
-      });
+      };
 
-      iElement.on('click', '.tp-close', function() {
-        var clickedTag = angular.element(this);
-        if (clickedTag.is('.focus')) {
-          clickedTag.removeClass('focus');
-        } else {
-          clickedTag.addClass('focus');
+      $scope.closeTag = function($event) {
+        $event.stopPropagation();
+        var closedTag = angular.element($event.target).parent()[0].childNodes[0].nodeValue.trim();
+        angular.forEach($scope.datasource, function(item, index) {
+          if (closedTag === item) {
+            $scope.datasource.splice(index, 1);
+          }
+        });
+      };
+
+      $scope.addTag = function($event) {
+        $event.stopPropagation();
+
+        if ($event.which === 13) {
+          var inputTag = angular.element($event.target);
+          var tagName = inputTag.val().trim();
+          if (tagName) {
+            $scope.datasource.push(tagName);
+            inputTag.val('');
+          }
         }
-      });
+      };
+
+      $scope.deleteTags = function($event) {
+
+        $event.stopPropagation();
+        var selectedTags = $element.find('.selected');
+        if ($event.which === 46 && selectedTags.length) {
+          var tagNames = [];
+          angular.forEach(selectedTags, function(item, index) {
+            tagNames.push(item.childNodes[0].nodeValue.trim());
+          });
+          $scope.datasource = $scope.datasource.filter(function(item) {
+            return tagNames.indexOf(item) < 0;
+          });
+        }
+      };
+
     }
   };
 });
