8b9dfb9949a7e65b37b486f84e782ad9d2b21e3d
dabeng <zhexiany@gmail.com>
2015-08-25 15:31:13 +0800

    Append the example of scope

---
+++ core-stuff-and-cool-stuff/controllers-and-scopes/scope-inheritance.js
@@ -0,0 +1,21 @@
+app.controller('ScopeInheritanceCtrl', function($scope, $element, $timeout) {
+  $scope.toggleTips = function($event) {
+    $scope.$emit('openTips', $event);
+  };
+  $timeout(function() {
+   angular.forEach($element.find('code'), function(block, index) {
+     hljs.highlightBlock(block);
+   });
+  }, 10);
+})
+.controller('MainController', function($scope, $rootScope) {
+  $rootScope.subject = "Today, we'll disscuss AngularJS.";
+  $scope.product = 'AngularJS';
+  $scope.job = 'front-end';
+})
+.controller('ChildController', function($scope) {
+  $scope.job = 'javascript';
+})
+.controller('GrandChildController', function($scope) {
+  $scope.job = 'MEAN.JS';
+});
